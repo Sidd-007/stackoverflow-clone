462e5f4a45137af516a7c5f7e10a1144316a945c
Sidd-007 <iamsiddhantmeshram@gmail.com>
2024-01-30 10:36:50 +0530

    added clerk webhook

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,13 @@
 import Question from "@/components/forms/Question"
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs"
-// import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
 
 const Page = async () => {
 
-    const userId = '123456'
+    const { userId } = auth()
 
-    // if (!userId) redirect('/sign-in')
+    if (!userId) redirect('/sign-in')
 
     const mongoUser = await getUserById({ userId })
 
@@ -17,7 +17,7 @@ const Page = async () => {
         <div>
             <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
             <div className="mt-9">
-                <Question mongoUserId={JSON.stringify(mongoUser._id)}/>
+                <Question mongoUserId={JSON.stringify(mongoUser._id)} />
             </div>
         </div>
     )
